e15bf209080db608558e32041c47a90b11c11351
Ngô Ngọc Bính <35886498+binhtlu@users.noreply.github.com>
2022-08-22 14:15:33 +0700

    test swangger dau tien thanh cong
    test swangger dau tien thanh cong

--- main.go
+++ main.go
@@ -1,163 +1,57 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
 	"net/http"
+
+	_ "go-gin-swag/docs"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-type User struct {
-	Id       string `json:"id"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
+// todo represents data about a task in the todo list
+type todo struct {
+	ID   string `json:"id"`
+	Task string `json:"task"`
 }
 
-//--- các phương thức thêm, sửa, xóa, tìm kiếm
-
-func (user User) ToString() string {
-	return fmt.Sprintf("id: %s\nName: %s\nPassword: %s\n", user.Id, user.Name, user.Password)
+// todo slice to seed todo list data
+var todoList = []todo{
+	{"1", "Learn Go"},
+	{"2", "Build an API with Go"},
+	{"3", "Document the API with swag"},
 }
 
-var (
-	listUser = make([]User, 0)
-)
-
-func CreateUser(user User) bool {
-	if user.Id != "" && user.Name != "" && user.Password != "" {
-		if userF, _ := FindUser(user.Id); 
-		userF == nil {
-			listUser = append(listUser, user)
-			return true
-		}
-	}
-	return false
+// @Summary get all items in the todo list
+// @ID get-all-todos
+// @Produce json
+// @Success 200 {object} todo
+// @Router /todo [get]
+func getAllTodos(c *gin.Context) {
+	c.JSON(http.StatusOK, todoList)
 }
 
-func FindUser(id string) (User, error) {
-	for _, user := range listUser {
-		if user.Id == id {
-			return user, nil
-		}
-	}
-	return nil, errors.New("user not found")
-}
+// @title Go + Gin Todo API
+// @version 1.0
+// @description This is a sample server todo server. You can visit the GitHub repository at https://github.com/LordGhostX/swag-gin-demo
 
-func UpdateUser(eUser User) bool {
-	for index, user := range listUser {
-		if user.Id == eUser.Id {
-			listUser[index] = eUser
-			return true
-		}
-	}
-	return false
-}
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
 
-func DeleteUser(id string) bool {
-	for index, user := range listUser {
-		if user.Id == id {
-			copy(listUser[index:], listUser[index+1:])
-			listUser[len(listUser)-1] = User{}
-			listUser = listUser[:len(listUser)-1]
-			return true
-		}
-	}
-	return false
-}
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
 
-func GetAllUser() []User {
-	return listUser
-}
-
-// Xử lý requet, response
-func FindUserapi(response http.ResponseWriter, request *http.Request) {
-	ids, ok := request.URL.Query()["id"]
-	if !ok || len(ids) < 1 {
-		responseWithError(response, http.StatusBadRequest, "url parm id is missing")
-		return
-	}
-	user, err := FindUser(ids[0])
-	if err != nil {
-		responseWithError(response, http.StatusInternalServerError, err.Error())
-		return
-	}
-	responseWithJSON(response, http.StatusOK, user)
-}
-
-func GetAll(response http.ResponseWriter, request *http.Request) {
-	users := GetAllUser()
-	responseWithJSON(response, http.StatusOK, users)
-}
-
-func CreateUserAPI(response http.ResponseWriter, request *http.Request) {
-	var user User
-	err := json.NewDecoder(request.Body).Decode(user)
-	if err != nil {
-		responseWithError(response, http.StatusBadRequest, err.Error())
-	} else {
-		result := CreateUser(user)
-		if !result {
-			responseWithError(response, http.StatusBadRequest, "Could not create user")
-			return
-		}
-		responseWithJSON(response, http.StatusOK, user)
-	}
-}
-
-func UpdateUserAPI(response http.ResponseWriter, request *http.Request) {
-	var user User
-	err := json.NewDecoder(request.Body).Decode(&user)
-	if err != nil {
-		responseWithError(response, http.StatusBadRequest, err.Error())
-	} else {
-		result := UpdateUser(user)
-		if !result {
-			responseWithError(response, http.StatusBadRequest, "Could not update user")
-			return
-		}
-		responseWithJSON(response, http.StatusOK, "Update user successfully")
-	}
-}
-
-func Delete(response http.ResponseWriter, request *http.Request) {
-	ids, ok := request.URL.Query()["id"]
-	if !ok || len(ids) < 1 {
-		responseWithError(response, http.StatusBadRequest, "Url Param id is missing")
-		return
-	}
-	result := DeleteUser(ids[0])
-	if !result {
-		responseWithError(response, http.StatusBadRequest, "Could not delete user")
-		return
-	}
-	responseWithJSON(response, http.StatusOK, "Delete user successfully")
-}
-
-func responseWithError(response http.ResponseWriter, statusCode int, msg string) {
-	responseWithJSON(response, statusCode, map[string]string{
-		"error": msg,
-	})
-}
-
-func responseWithJSON(response http.ResponseWriter, statusCode int, data interface{}) {
-	result, _ := json.Marshal(data)
-	response.Header().Set("Content-Type", "application/json")
-	response.WriteHeader(statusCode)
-	response.Write(result)
-}
-
+// @host localhost:8080
+// @BasePath /
+// @query.collection.format multi
 func main() {
-	router := NewRouter()
-
-	router.HandleFunc("/api/v1/user/find", FindUser).Methods("GET")
-	router.HandleFunc("/api/v1/user/getall", GetAll).Methods("GET")
-	router.HandleFunc("/api/v1/user/create", CreateUser).Methods("POST")
-	router.HandleFunc("/api/v1/user/update", UpdateUser).Methods("PUT")
-	router.HandleFunc("/api/v1/user/delete", Delete).Methods("DELETE")
-
-	err := http.ListenAndServe(":5000", router)
-	if err != nil {
-		panic(err)
-	}
-
+	// configure the Gin server
+	router := gin.Default()
+	//docs.SwaggerInfo.BasePath = "/api/v1"
+	router.GET("/todo", getAllTodos)
+	// run the Gin server
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	router.Run("localhost:8080")
 }
